429d912c47724186592680dae364b423c14ba02f
hugobessaa <hugobessaa@gmail.com>
2016-05-18 00:23:38 +0000

    First commit

---
+++ main.hs
@@ -0,0 +1,143 @@
+{-# LANGUAGE OverloadedStrings, TypeFamilies, QuasiQuotes,
+             TemplateHaskell, GADTs, FlexibleInstances,
+             MultiParamTypeClasses, DeriveDataTypeable,
+             GeneralizedNewtypeDeriving, ViewPatterns, EmptyDataDecls #-}
+import Yesod
+import Database.Persist.Postgresql
+import Data.Text
+import Control.Monad.Logger (runStdoutLoggingT)
+
+data Pagina = Pagina{connPool :: ConnectionPool}
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Users json
+   nome Text
+   login Text
+   senha Text
+   deriving Show
+|]
+
+mkYesod "Pagina" [parseRoutes|
+/ HomeR GET
+/login LoginR GET POST
+/erro ErroR GET
+/usuario UsuarioR GET POST
+/perfil/#UsersId PerfilR GET
+/admin AdminR GET
+/logout LogoutR GET
+|]
+
+instance Yesod Pagina where
+    authRoute _ = Just LoginR
+    
+    isAuthorized LoginR _ = return Authorized
+    isAuthorized ErroR _ = return Authorized
+    isAuthorized HomeR _ = return Authorized
+    isAuthorized UsuarioR _ = return Authorized
+    isAuthorized AdminR _ = isAdmin
+    isAuthorized _ _ = isUser
+
+isUser = do
+    mu <- lookupSession "_ID"
+    return $ case mu of
+        Nothing -> AuthenticationRequired
+        Just _ -> Authorized
+    
+isAdmin = do
+    mu <- lookupSession "_ID"
+    return $ case mu of
+        Nothing -> AuthenticationRequired
+        Just "admin" -> Authorized 
+        Just _ -> Unauthorized "Voce precisa ser admin para entrar aqui"
+
+instance YesodPersist Pagina where
+   type YesodPersistBackend Pagina = SqlBackend
+   runDB f = do
+       master <- getYesod
+       let pool = connPool master
+       runSqlPool f pool
+
+type Form a = Html -> MForm Handler (FormResult a, Widget)
+
+instance RenderMessage Pagina FormMessage where
+    renderMessage _ _ = defaultFormMessage
+
+formUser :: Form Users
+formUser = renderDivs $ Users <$>
+           areq textField "Nome: " Nothing <*>
+           areq textField "Login: " Nothing <*>
+           areq passwordField "Senha: " Nothing
+
+formLogin :: Form (Text,Text)
+formLogin = renderDivs $ (,) <$>
+           areq textField "Login: " Nothing <*>
+           areq passwordField "Senha: " Nothing
+
+getUsuarioR :: Handler Html
+getUsuarioR = do
+           (widget, enctype) <- generateFormPost formUser
+           defaultLayout [whamlet|
+                 <form method=post enctype=#{enctype} action=@{UsuarioR}>
+                     ^{widget}
+                     <input type="submit" value="Enviar">
+           |]
+
+getPerfilR :: UsersId -> Handler Html
+getPerfilR uid = do
+      user <- runDB $ get404 uid
+      defaultLayout [whamlet|
+          <p><b> Pagina de #{usersNome user}
+          <p><b> Login: #{usersLogin user}
+      |]
+
+postUsuarioR :: Handler Html
+postUsuarioR = do
+           ((result, _), _) <- runFormPost formUser
+           case result of 
+               FormSuccess user -> (runDB $ insert user) >>= \piid -> redirect (PerfilR piid)
+               _ -> redirect ErroR
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Hello World!|]
+
+getAdminR :: Handler Html
+getAdminR = defaultLayout [whamlet|Hello World!|]
+
+getLoginR :: Handler Html
+getLoginR = do
+           (widget, enctype) <- generateFormPost formLogin
+           defaultLayout [whamlet|
+                 <form method=post enctype=#{enctype} action=@{LoginR}>
+                     ^{widget}
+                     <input type="submit" value="Login">
+           |]
+
+postLoginR :: Handler Html
+postLoginR = do
+           ((result, _), _) <- runFormPost formLogin
+           case result of 
+               FormSuccess ("admin","admin") -> setSession "_ID" "admin" >> redirect AdminR
+               FormSuccess (login,senha) -> do 
+                   user <- runDB $ selectFirst [UsersLogin ==. login, UsersSenha ==. senha] []
+                   case user of
+                       Nothing -> redirect LoginR
+                       Just (Entity pid u) -> setSession "_ID" (pack $ show $ fromSqlKey pid) >> redirect (PerfilR pid)
+
+getErroR :: Handler Html
+getErroR = defaultLayout [whamlet|
+     <h1> Erro de cadastro
+|]
+
+getLogoutR :: Handler Html
+getLogoutR = do
+     deleteSession "_ID"
+     defaultLayout [whamlet| 
+         <h1> ADEUS!
+     |]
+
+connStr = "dbname=dd9en8l5q4hh2a host=ec2-107-21-219-201.compute-1.amazonaws.com user=kpuwtbqndoeyqb password=aCROh525uugAWF1l7kahlNN3E0 port=5432"
+
+main::IO()
+main = runStdoutLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do 
+       runSqlPersistMPool (runMigration migrateAll) pool
+       warp 8080 (Pagina pool)
