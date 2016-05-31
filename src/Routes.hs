351fccbae02a29e54fda18a02ae372deb3939aed
Hugo Bessa <eu@hugobessa.com.br>
2016-05-31 18:21:45 -0300

    Some additions to Routes

--- src/Routes.hs
+++ src/Routes.hs
@@ -19,14 +19,10 @@ pRoutes = [parseRoutes|
 /logout LogoutR GET
 /error ErrorR GET
 
-/api/authors AuthorsR GET POST
-/api/authors/#AuthorId AuthorR GET PUT DELETE
-
-/api/posts PostsR GET POST
-/api/posts/#PostId PostR GET PUT DELETE
-
 /api/tags TagsR GET POST
 /api/tags/#TagId TagR GET PUT DELETE
+
+/api/posts/#PostId PostR GET PUT DELETE
 
 /api/collections CollectionsR GET POST
 /api/collections/#CollectionId CollectionR GET PUT DELETE
